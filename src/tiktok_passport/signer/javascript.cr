0ab751ef828d16d9f355ef576e1f268dbbe16a42
kandayo <kdy@absolab.xyz>
2021-04-21 02:09:12 -0300

    feat: use new byted_acrawler function

--- src/tiktok_passport/signer/javascript.cr
+++ src/tiktok_passport/signer/javascript.cr
@@ -4,6 +4,10 @@ module TiktokPassport
       def self.sign(url : String) : String
         %Q[return byted_acrawler.sign({ url: "#{url}" });]
       end
+
+      def self.register_function
+        %q[{{ read_file("#{__DIR__}/javascript/function.js") }}]
+      end
     end
   end
 end
