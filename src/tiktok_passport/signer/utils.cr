f0b77d145ca09968b0ce9489f28f07b407b911bc
kandayo <kdy@absolab.xyz>
2021-04-24 00:30:18 -0300

    refactor tiktok verify_fp generation algorithm

--- src/tiktok_passport/signer/utils.cr
+++ src/tiktok_passport/signer/utils.cr
@@ -1,12 +1,13 @@
 module TiktokPassport
   module Signer
     module Utils
+      VERIFY_FP_SEED = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+      # Generates a valid `verifyFp` using the Tiktok algorithm.
       # https://github.com/davidteather/TikTok-Api/issues/347
       def self.generate_verify_fp : String
-        dict = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-        dict_length = dict.size
         timestamp = Time.utc.to_unix_ms.to_s(36)
-        uuid = Array.new(36, '0')
+        uuid = StaticArray(Char, 36).new('0')
 
         uuid[8] = uuid[13] = uuid[18] = uuid[23] = '_'
         uuid[14] = '4'
@@ -14,10 +15,10 @@ module TiktokPassport
         36.times do |index|
           next if uuid[index] != '0'
 
-          random = (Random.rand * dict_length).to_i
+          random = (Random.rand * VERIFY_FP_SEED.size).to_i
           char_index = ((3 & random) | (index == 19 ? 8 : random)).to_i
 
-          uuid[index] = dict[char_index]
+          uuid[index] = VERIFY_FP_SEED[char_index]
         end
 
         "verify_#{timestamp.downcase}_#{uuid.join}"
