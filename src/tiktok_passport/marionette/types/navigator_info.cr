a7f716e40300d43dcbb506c2b5572c420aacc10e
kandayo <kdy@absolab.xyz>
2021-04-24 05:08:39 -0300

    cosmetics: reorder Marionette::NavigatorInfo properties

--- src/tiktok_passport/marionette/types/navigator_info.cr
+++ src/tiktok_passport/marionette/types/navigator_info.cr
@@ -4,12 +4,12 @@ module TiktokPassport
       include JSON::Serializable
 
       getter user_agent : String
-      getter screen_width : Int32
-      getter screen_height : Int32
-      getter browser_language : String
-      getter browser_platform : String
       getter browser_name : String
       getter browser_version : String
+      getter browser_language : String
+      getter browser_platform : String
+      getter screen_width : Int32
+      getter screen_height : Int32
     end
   end
 end
