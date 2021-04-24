36975dadddc76b417e643ec6cbc768e15c2aabf9
kandayo <kdy@absolab.xyz>
2021-04-24 00:43:00 -0300

    add example/stealth_test.cr

---
+++ examples/stealth_test.cr
@@ -0,0 +1,11 @@
+require "../src/tiktok_passport"
+
+marionette = TiktokPassport::Signer::Marionette.new(ENV["SELENIUM_BROWSER_URL"])
+
+marionette.navigate_to("https://bot.sannysoft.com/")
+marionette.screenshot("self-test-#{Time.utc.to_unix_ms}-01.jpg")
+
+marionette.navigate_to("https://arh.antoinevastel.com/bots/areyouheadless")
+marionette.screenshot("self-test-#{Time.utc.to_unix_ms}-02.jpg")
+
+marionette.stop
