9f4075959510a548172810899ed3aa49c1b32dbe
kandayo <kdy@absolab.xyz>
2021-04-24 00:26:59 -0300

    escape javascript from byted_acrawler.sign params

--- spec/tiktok_passport/signer/javascript_spec.cr
+++ spec/tiktok_passport/signer/javascript_spec.cr
@@ -1,11 +1,74 @@
 require "../../spec_helper"
 
 describe TiktokPassport::Signer::Javascript do
+  describe ".escape_javascript" do
+    it "escapes javascript" do
+      TiktokPassport::Signer::Javascript
+        .escape_javascript(%(string))
+        .should eq(
+          %(string)
+        )
+
+      TiktokPassport::Signer::Javascript
+        .escape_javascript(%(This "thing" is really\n netos'))
+        .should eq(
+          %(This \\"thing\\" is really\\n netos\\')
+        )
+
+      TiktokPassport::Signer::Javascript
+        .escape_javascript(%(backslash\\test))
+        .should eq(
+          %(backslash\\\\test)
+        )
+
+      TiktokPassport::Signer::Javascript
+        .escape_javascript(%(dont </close> tags))
+        .should eq(
+          %(dont <\\/close> tags)
+        )
+
+      TiktokPassport::Signer::Javascript
+        .escape_javascript(%(unicode \u2028 newline))
+        .should eq(
+          %(unicode &#x2028; newline)
+        )
+
+      TiktokPassport::Signer::Javascript
+        .escape_javascript(%(unicode \342\200\250 newline))
+        .should eq(
+          %(unicode &#x2028; newline)
+        )
+
+      TiktokPassport::Signer::Javascript
+        .escape_javascript(%(unicode \u2029 newline))
+        .should eq(
+          %(unicode &#x2029; newline)
+        )
+
+      TiktokPassport::Signer::Javascript
+        .escape_javascript(%(unicode \342\200\251 newline))
+        .should eq(
+          %(unicode &#x2029; newline)
+        )
+    end
+  end
+
   describe ".sign" do
     it "returns the sign function" do
       result = TiktokPassport::Signer::Javascript.sign("http://tiktok.com")
       result.should eq(
-        %Q[return byted_acrawler.sign({ url: "http://tiktok.com" });]
+        <<-JS
+          return byted_acrawler.sign({ url: "http://tiktok.com" });
+        JS
+      )
+    end
+
+    it "escapes javascript" do
+      result = TiktokPassport::Signer::Javascript.sign("http://tiktok.com\"<b>abc</b>")
+      result.should eq(
+        <<-JS
+          return byted_acrawler.sign({ url: \"http://tiktok.com\\\"<b>abc<\\/b>\" });
+        JS
       )
     end
   end
--- src/tiktok_passport/signer/javascript.cr
+++ src/tiktok_passport/signer/javascript.cr
@@ -1,8 +1,31 @@
 module TiktokPassport
   module Signer
     module Javascript
+      JAVASCRIPT_ESCAPE_REGEX = /(\\|<\/|\r\n|\x{2028}|\x{2029}|[\n\r"']|[`]|[$])/
+      JAVASCRIPT_ESCAPE_MAP   = {
+        "\\"           => "\\\\",
+        "</"           => "<\\/",
+        "\r\n"         => "\\n",
+        "\n"           => "\\n",
+        "\r"           => "\\n",
+        "\""           => "\\\"",
+        "'"            => "\\'",
+        "`"            => "\\`",
+        "$"            => "\\$",
+        "\342\200\250" => "&#x2028;",
+        "\342\200\251" => "&#x2029;",
+      }
+
+      def self.escape_javascript(script : String)
+        return "" if script.blank?
+
+        script.gsub(JAVASCRIPT_ESCAPE_REGEX, JAVASCRIPT_ESCAPE_MAP)
+      end
+
       def self.sign(url : String) : String
-        %Q[return byted_acrawler.sign({ url: "#{url}" });]
+        <<-JS
+          return byted_acrawler.sign({ url: "#{escape_javascript(url)}" });
+        JS
       end
 
       def self.register_function
